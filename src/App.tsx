fb02a59809235e22393d6bbccc22ef19aafcca33
amit cohen <cohen415@walla.com>
2023-11-23 19:49:21 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='app-container'>
+      <form className='note-form'>
+        <input placeholder='title' required>
+        </input>
+        <textarea placeholder='Content' rows={10} required>
+        </textarea>
+        <button type='submit'>
+          Add Note
+        </button>
+      </form>
+      <div className='notes-grid'>
+        <div className='note-item'>
+          <div className='notes-header'>
+            <button>X</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note Content</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
